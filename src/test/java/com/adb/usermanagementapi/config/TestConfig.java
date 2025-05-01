514c39f3ebbd653be1f6a3f3eed071e8cd6fdf04
EmmaAdabra <adabraemma@gmail.com>
2025-05-01 12:29:57 +0100

    Add TestConfig.java to provide test root application context
    - Registers non-web beans like services and repositories for test context

---
+++ src/test/java/com/adb/usermanagementapi/config/TestConfig.java
@@ -0,0 +1,34 @@
+package com.adb.usermanagementapi.config;
+
+import com.adb.usermanagementapi.repository.UserRepository;
+import com.adb.usermanagementapi.repository.UserRepositoryImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class TestConfig {
+    // adding h2 database bean
+    @Bean
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("schema.sql")
+                .setName("userDb")
+                .build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public UserRepository userRepository(JdbcTemplate jdbcTemplate){
+        return new UserRepositoryImpl(jdbcTemplate);
+    }
+}
