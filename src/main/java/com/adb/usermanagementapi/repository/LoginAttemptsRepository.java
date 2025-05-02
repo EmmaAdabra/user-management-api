45401c6456aa36bfcca0d84ed52fc163d4624e51
EmmaAdabra <adabraemma@gmail.com>
2025-05-02 21:44:15 +0100

    Add lastFailedLoginAttempt method contract to LoginAttemptsRepository interface
    - It accept user ID (Long)
    - Return time of a user last failed login attempt

--- src/main/java/com/adb/usermanagementapi/repository/LoginAttemptsRepository.java
+++ src/main/java/com/adb/usermanagementapi/repository/LoginAttemptsRepository.java
@@ -1,6 +1,9 @@
 package com.adb.usermanagementapi.repository;
 
+import java.sql.Timestamp;
+
 public interface LoginAttemptsRepository {
     int countFailedAttemptsInLastTwoMinutes(Long userId);
     public void logLoginAttempt(Long userId, boolean success);
+    public Timestamp lastFailedLoginAttempt(Long user_id);
 }
