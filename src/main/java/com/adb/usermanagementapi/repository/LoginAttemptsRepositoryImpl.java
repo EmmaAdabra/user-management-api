f99b8fc99822d0da4ea787533002d0c5b3c6f2bd
EmmaAdabra <adabraemma@gmail.com>
2025-05-02 21:45:35 +0100

    Implement lastFailedLoginAttempt in LoginAttemptsRepositoryImpl
    - It accept user ID (Long)
    - Return time of a user last failed login attempt
    - Returns null if user have no failed login attempt

--- src/main/java/com/adb/usermanagementapi/repository/LoginAttemptsRepositoryImpl.java
+++ src/main/java/com/adb/usermanagementapi/repository/LoginAttemptsRepositoryImpl.java
@@ -1,7 +1,9 @@
 package com.adb.usermanagementapi.repository;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 public class LoginAttemptsRepositoryImpl implements LoginAttemptsRepository{
@@ -23,5 +25,16 @@ public class LoginAttemptsRepositoryImpl implements LoginAttemptsRepository{
     public void logLoginAttempt(Long userId, boolean success) {
         String sql = "INSERT INTO login_attempts (user_id, attempt_time, success) VALUES (?, ?, ?)";
         jdbcTemplate.update(sql, userId, LocalDateTime.now(), success);
+    }
+
+    @Override
+    public Timestamp lastFailedLoginAttempt(Long user_id) {
+        String sql = "SELECT MAX(attempt_time) FROM login_attempts WHERE user_id = ? AND success " +
+                "= false";
+        try {
+            return jdbcTemplate.queryForObject(sql, Timestamp.class, user_id);
+        } catch (EmptyResultDataAccessException e){
+            return null;
+        }
     }
 }
