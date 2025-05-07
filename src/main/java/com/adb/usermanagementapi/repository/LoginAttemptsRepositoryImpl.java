8eaf8925fc5758c232035cb9e0bae499062d36f0
EmmaAdabra <adabraemma@gmail.com>
2025-05-07 09:49:38 +0100

    Refactor(LoginAttemptsRepositoryImpl): move raw sql queries within methods to static constant fields.
    - Ensure clarity and maintainability

--- src/main/java/com/adb/usermanagementapi/repository/LoginAttemptsRepositoryImpl.java
+++ src/main/java/com/adb/usermanagementapi/repository/LoginAttemptsRepositoryImpl.java
@@ -8,6 +8,12 @@ import java.time.LocalDateTime;
 
 public class LoginAttemptsRepositoryImpl implements LoginAttemptsRepository{
     private final JdbcTemplate jdbcTemplate;
+    private static final String COUNT_USER_FAILED_LOGIN_WITHIN_GIVEN_TIME =  "SELECT COUNT(*) " +
+            "FROM login_attempts WHERE user_id = ? AND success = ? AND attempt_time >= ?";
+    private static final String INSERT_INTO_LOGIN_ATTEMPTS_BY_ID = "INSERT INTO login_attempts " +
+            "(user_id, attempt_time, success) VALUES (?, ?, ?)";
+    private static final String SELECT_LAST_FAILED_ATTEMPTS_TIME = "SELECT MAX(attempt_time) FROM login_attempts WHERE user_id = ? AND success " +
+            "= false";
 
     public LoginAttemptsRepositoryImpl(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -15,24 +21,19 @@ public class LoginAttemptsRepositoryImpl implements LoginAttemptsRepository{
 
     @Override
     public int countFailedAttemptsInLastTwoMinutes(Long userId) {
-        String sql = "SELECT COUNT(*) FROM login_attempts WHERE user_id = ? AND success = ? AND " +
-                "attempt_time >= ?";
         LocalDateTime twoMinutesAgo = LocalDateTime.now().minusMinutes(2);
-        return jdbcTemplate.queryForObject(sql, Integer.class, userId, false, twoMinutesAgo);
+        return jdbcTemplate.queryForObject(COUNT_USER_FAILED_LOGIN_WITHIN_GIVEN_TIME, Integer.class, userId, false, twoMinutesAgo);
     }
 
     @Override
     public void logLoginAttempt(Long userId, boolean success) {
-        String sql = "INSERT INTO login_attempts (user_id, attempt_time, success) VALUES (?, ?, ?)";
-        jdbcTemplate.update(sql, userId, LocalDateTime.now(), success);
+        jdbcTemplate.update(INSERT_INTO_LOGIN_ATTEMPTS_BY_ID, userId, LocalDateTime.now(), success);
     }
 
     @Override
     public Timestamp lastFailedLoginAttempt(Long user_id) {
-        String sql = "SELECT MAX(attempt_time) FROM login_attempts WHERE user_id = ? AND success " +
-                "= false";
         try {
-            return jdbcTemplate.queryForObject(sql, Timestamp.class, user_id);
+            return jdbcTemplate.queryForObject(SELECT_LAST_FAILED_ATTEMPTS_TIME, Timestamp.class, user_id);
         } catch (EmptyResultDataAccessException e){
             return null;
         }
