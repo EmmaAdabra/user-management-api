11bb2910be2136a90809c7bf2d677a4a8b87ee29
EmmaAdabra <adabraemma@gmail.com>
2025-05-09 09:50:42 +0100

    Rename LoginAttempts to LoginAttempt

--- src/main/java/com/adb/usermanagementapi/model/dto/LoginAttempt.java
+++ src/main/java/com/adb/usermanagementapi/model/dto/LoginAttempt.java
@@ -2,7 +2,7 @@ package com.adb.usermanagementapi.model.dto;
 
 import java.time.LocalDateTime;
 
-public record LoginAttempts(
+public record LoginAttempt(
         Long id,
         Long userId,
         LocalDateTime attemptTime,
