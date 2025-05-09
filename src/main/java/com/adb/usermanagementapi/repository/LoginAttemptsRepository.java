f1d7fc55177e9ed92646ab90593e3903399026e8
EmmaAdabra <adabraemma@gmail.com>
2025-05-09 13:09:38 +0100

    Add a new method contract findRecentLogins to LoginAttemptsRepository interface.
    - It accept Long userId, int timeInterval and returns list of LoginAttempt dto

--- src/main/java/com/adb/usermanagementapi/repository/LoginAttemptsRepository.java
+++ src/main/java/com/adb/usermanagementapi/repository/LoginAttemptsRepository.java
@@ -1,9 +1,13 @@
 package com.adb.usermanagementapi.repository;
 
+import com.adb.usermanagementapi.model.dto.LoginAttempt;
+
 import java.sql.Timestamp;
+import java.util.List;
 
 public interface LoginAttemptsRepository {
     int countFailedAttemptsInLastTwoMinutes(Long userId);
     public void logLoginAttempt(Long userId, boolean success);
     public Timestamp lastFailedLoginAttempt(Long user_id);
+    List<LoginAttempt> findRecentLogins(Long userId, int timeInterval);
 }
