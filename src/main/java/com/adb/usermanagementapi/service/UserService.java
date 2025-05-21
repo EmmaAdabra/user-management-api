06787ac26000ec63f534f5ddbc50b90545c933e2
EmmaAdabra <adabraemma@gmail.com>
2025-05-21 20:29:11 +0100

    Declare method signatures in UserService interface

--- src/main/java/com/adb/usermanagementapi/service/UserService.java
+++ src/main/java/com/adb/usermanagementapi/service/UserService.java
@@ -1,3 +1,16 @@
 package com.adb.usermanagementapi.service;
 
-public class UserService {}
+import com.adb.usermanagementapi.dto.request.UserCreateRequestDTO;
+import com.adb.usermanagementapi.dto.request.UserUpdateDTO;
+import com.adb.usermanagementapi.dto.response.UserResponseDTO;
+
+import java.util.List;
+
+public interface UserService {
+    UserResponseDTO createUser(UserCreateRequestDTO request);
+    UserResponseDTO updateUser(Long id, UserUpdateDTO request);
+    UserResponseDTO getUserById(Long id);
+    List<UserResponseDTO> getAllUsers();
+    void updatePassword(Long id, String newPassword);
+    void deleteUser(Long id);
+}
