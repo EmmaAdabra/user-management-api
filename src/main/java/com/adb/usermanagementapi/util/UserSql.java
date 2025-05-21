e4bcfcbc99ba764052c61a0d51954e9a49f54c3d
EmmaAdabra <adabraemma@gmail.com>
2025-05-21 20:18:38 +0100

    Rename:   - SELECT_USER_BY_USERNAME   - UPDATE_USER_PASSWORD_BY_USERNAME   - DELETE_USER_BY_USERNAME.  To:   - SELECT_USER_BY_ID   - UPDATE_USER_PASSWORD_BY_ID   - DELETE_USER_BY_ID
     Modify:
      - SELECT_USER_BY_ID
      - UPDATE_USER_PASSWORD_BY_ID
      - DELETE_USER_BY_ID
      - IS_USER_LOCKED
      - UPDATE_USER_LOCK_STATUS
      sql query to reference id not username column

--- src/main/java/com/adb/usermanagementapi/util/UserSql.java
+++ src/main/java/com/adb/usermanagementapi/util/UserSql.java
@@ -13,13 +13,16 @@ public class UserSql {
             "= ? LIMIT 1";
     public static final String SELECT_USER_BY_USERNAME = "SELECT id, username, email, " +
             "password_hash, created_at, is_locked FROM users WHERE username = ?";
+    public static final String SELECT_USER_BY_ID = "SELECT id, username, email, " +
+            "password_hash, created_at, is_locked FROM users WHERE id = ?";
     public static final String SELECT_ALL_USERS = "SELECT id, username, email, password_hash, " +
             "created_at, is_locked FROM users";
     public static final String UPDATE_USER_USERNAME_EMAIL_BY_ID = "UPDATE users SET username = ?," +
             " email = ? WHERE id = ?";
-    public static final String UPDATE_USER_PASSWORD_BY_USERNAME = "UPDATE users SET password_hash" +
-            " = ? WHERE username = ?";
-    public static final String DELETE_USER_BY_USERNAME = "DELETE FROM users WHERE username = ?";
-    public static final String IS_USER_LOCKED = "SELECT is_locked FROM users WHERE username = ?";
-    public static final String UPDATE_USER_LOCK_STATUS = "UPDATE users SET is_locked = ? WHERE username = ?";
+    public static final String UPDATE_USER_PASSWORD_BY_ID = "UPDATE users SET password_hash" +
+            " = ? WHERE id = ?";
+    public static final String DELETE_USER_BY_ID = "DELETE FROM users WHERE id = ?";
+    public static final String IS_USER_LOCKED = "SELECT is_locked FROM users WHERE id = ?";
+    public static final String UPDATE_USER_LOCK_STATUS = "UPDATE users SET is_locked = ? WHERE id" +
+            " = ?";
 }
