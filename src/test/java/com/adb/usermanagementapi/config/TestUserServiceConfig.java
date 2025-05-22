52f745215e6da0604148a9a4e837b38c15da71ae
EmmaAdabra <adabraemma@gmail.com>
2025-05-22 13:40:33 +0100

    Add TestUserServiceConfig spring configuration file for user service test context.

---
+++ src/test/java/com/adb/usermanagementapi/config/TestUserServiceConfig.java
@@ -0,0 +1,9 @@
+package com.adb.usermanagementapi.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.adb.usermanagementapi.service")
+public class TestUserServiceConfig {
+}
