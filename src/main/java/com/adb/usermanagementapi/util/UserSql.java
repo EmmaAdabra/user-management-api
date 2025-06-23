87d70624a200c9d6766a9571beb0a9174d6f0528
EmmaAdabra <adabraemma@gmail.com>
2025-06-23 22:55:34 +0100

    Modify SELECT_ALL_USERS in UserSql query to use Offset and limit clause
    - This is to implement pagination while fetching users.

--- src/main/java/com/adb/usermanagementapi/util/UserSql.java
+++ src/main/java/com/adb/usermanagementapi/util/UserSql.java
@@ -18,7 +18,7 @@ public class UserSql {
     public static final String SELECT_USER_BY_EMAIL = "SELECT id, username, email, " +
             "password_hash, created_at, is_locked FROM users WHERE email = ?";
     public static final String SELECT_ALL_USERS = "SELECT id, username, email, password_hash, " +
-            "created_at, is_locked FROM users";
+            "created_at, is_locked FROM users ORDER BY id LIMIT ? OFFSET ?";
     public static final String UPDATE_USER_USERNAME_EMAIL_BY_ID = "UPDATE users SET username = ?," +
             " email = ? WHERE id = ?";
     public static final String UPDATE_USER_PASSWORD_BY_ID = "UPDATE users SET password_hash" +
