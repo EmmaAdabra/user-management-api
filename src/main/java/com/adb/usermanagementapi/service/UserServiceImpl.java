9cd3646531e0014346009a1406d2be29f9d7183f
EmmaAdabra <adabraemma@gmail.com>
2025-06-26 17:10:56 +0100

    Modify updatePassword in UserServiceImpl
    - Add a new dependency - PasswordHasher and update constructor to initialize it.
    - modify updatePassword signature to accept ChangePasswordRequestDTO object instead of string.
    - Add a try-catch block to updatePassword to catch InvalidCurrentPasswordException

--- src/main/java/com/adb/usermanagementapi/service/UserServiceImpl.java
+++ src/main/java/com/adb/usermanagementapi/service/UserServiceImpl.java
@@ -1,13 +1,17 @@
 package com.adb.usermanagementapi.service;
 
+import com.adb.usermanagementapi.dto.request.ChangePasswordRequestDTO;
 import com.adb.usermanagementapi.dto.request.UserCreateRequestDTO;
 import com.adb.usermanagementapi.dto.request.UserUpdateDTO;
 import com.adb.usermanagementapi.dto.response.UserResponseDTO;
 import com.adb.usermanagementapi.exception.DuplicateResourceException;
+import com.adb.usermanagementapi.exception.InvalidCurrentPasswordException;
+import com.adb.usermanagementapi.exception.InvalidPasswordException;
 import com.adb.usermanagementapi.exception.UserNotFoundException;
 import com.adb.usermanagementapi.model.User;
 import com.adb.usermanagementapi.repository.UserRepository;
 import com.adb.usermanagementapi.mapper.UserMapper;
+import com.adb.usermanagementapi.service.security.PasswordHasher;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,10 +24,13 @@ import java.util.stream.Collectors;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordHasher passwordHasher;
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper,
+                           PasswordHasher passordHasher) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordHasher = passordHasher;
     }
 
     @Override
@@ -85,11 +92,23 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void updatePassword(Long id, String newPassword) {
-        userRepository.findById(id)
+    public void updatePassword(Long id, ChangePasswordRequestDTO dto) {
+        User user = userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException("User not found"));
 
-        String hashedPassword = BCrypt.hashpw(newPassword, BCrypt.gensalt(12));
+        try {
+            passwordHasher.validate(user, dto.getOldPassword());
+        } catch (InvalidPasswordException e) {
+            throw new InvalidCurrentPasswordException("Current password mismatch");
+        }
+
+        if (dto.getOldPassword().equals(dto.getNewPassword())) {
+            throw new InvalidPasswordException("New password must be different from the current password");
+        }
+
+        String hashedPassword = passwordHasher.hashPassword(dto.getNewPassword());
+        System.out.println("Test value: " + dto.getNewPassword());
+        System.out.println(hashedPassword);
         userRepository.updatePassword(id, hashedPassword);
     }
 
