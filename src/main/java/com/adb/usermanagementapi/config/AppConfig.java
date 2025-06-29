82c50aff73aad623965d04915c98283a5d680d86
EmmaAdabra <adabraemma@gmail.com>
2025-06-29 17:37:41 +0100

    Add @EnableAspectJAutoProxy to AppConfig, to enable spring AOP

--- src/main/java/com/adb/usermanagementapi/config/AppConfig.java
+++ src/main/java/com/adb/usermanagementapi/config/AppConfig.java
@@ -5,6 +5,7 @@ import com.adb.usermanagementapi.repository.UserRepositoryImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -12,6 +13,7 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import javax.sql.DataSource;
 
 @Configuration
+@EnableAspectJAutoProxy
 @ComponentScan(basePackages = "com.adb.usermanagementapi")
 public class AppConfig {
     // adding h2 database bean
