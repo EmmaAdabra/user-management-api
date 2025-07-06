771cd5418a144936e6cebd8514d99bb89201ef2d
EmmaAdabra <adabraemma@gmail.com>
2025-07-06 19:33:41 +0100

    Add LoginController, handles login request and response

---
+++ src/main/java/com/adb/usermanagementapi/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.adb.usermanagementapi.controller;
+
+import com.adb.usermanagementapi.dto.request.LoginRequestDTO;
+import com.adb.usermanagementapi.dto.response.UserResponseDTO;
+import com.adb.usermanagementapi.service.security.LoginService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class LoginController {
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserResponseDTO> login(@Valid @RequestBody LoginRequestDTO request){
+        UserResponseDTO response = loginService.login(request);
+
+        return ResponseEntity.ok(response);
+    }
+}
