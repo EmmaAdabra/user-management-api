b97cb26e34c52d5d9c46e05d41d2ac0bb976c6dd
EmmaAdabra <adabraemma@gmail.com>
2025-07-07 00:09:21 +0100

    Add logs to LoginController

--- src/main/java/com/adb/usermanagementapi/controller/LoginController.java
+++ src/main/java/com/adb/usermanagementapi/controller/LoginController.java
@@ -3,7 +3,10 @@ package com.adb.usermanagementapi.controller;
 import com.adb.usermanagementapi.dto.request.LoginRequestDTO;
 import com.adb.usermanagementapi.dto.response.UserResponseDTO;
 import com.adb.usermanagementapi.service.security.LoginService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,13 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class LoginController {
     private final LoginService loginService;
+    private final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     public LoginController(LoginService loginService) {
         this.loginService = loginService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<UserResponseDTO> login(@Valid @RequestBody LoginRequestDTO request){
+    public ResponseEntity<UserResponseDTO> login(@Valid @RequestBody
+             LoginRequestDTO request, HttpServletRequest servletRequest)
+    {
+        String ip = servletRequest.getRemoteAddr();
+        String url = servletRequest.getRequestURI();
+        String httpMethod = servletRequest.getMethod();
+
+        logger.info("Login attempt - IP: {}, method: {}, URL: {}, email: {}", ip, httpMethod, url
+                , request.getEmail());
         UserResponseDTO response = loginService.login(request);
 
         return ResponseEntity.ok(response);
