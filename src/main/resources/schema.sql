acad7d3361ef0bf76ef2dbf0e6941747f07c55a0
EmmaAdabra <adabraemma@gmail.com>
2025-07-21 13:07:06 +0100

    Modify sql script to follow SQLite syntax

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,23 @@
-CREATE TABLE users(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(50) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    is_locked BOOLEAN DEFAULT FALSE
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS login_attempts;
+
+
+CREATE TABLE users (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   username TEXT NOT NULL UNIQUE,
+   email TEXT NOT NULL UNIQUE,
+   password_hash TEXT NOT NULL,
+   created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+   is_locked INTEGER DEFAULT 0
 );
 
-CREATE TABLE login_attempts(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    attempt_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    success BOOLEAN NOT NULL,
+CREATE TABLE login_attempts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    attempt_time TEXT DEFAULT CURRENT_TIMESTAMP,
+    success INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
--- index for login_attempts columns (user_id, attempt_time, success)
 CREATE INDEX idx_login_attempts_user_id_attempt_time_success
-ON login_attempts(user_id, attempt_time, success);
+    ON login_attempts(user_id, attempt_time, success);
